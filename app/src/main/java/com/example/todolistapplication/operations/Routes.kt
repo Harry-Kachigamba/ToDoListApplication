9636001284a4645f2d772411259efb9e07358ffa
Harry Kachigamba <harrykachigamba22@outlook.com>
2025-01-22 00:50:41 +0200

    deleted

--- app/src/main/java/com/example/todolistapplication/operations/Routes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.todolistapplication.operations
-
-open class Routes(val route: String) {
-    object ToDoList : Routes("list")
-    object AddEditToDo : Routes("addEdit?todoId={todoId}") {
-        fun createRoute(todoId: Int?) = "addEdit?todoId=${todoId ?: -1}"
-    }
-}
