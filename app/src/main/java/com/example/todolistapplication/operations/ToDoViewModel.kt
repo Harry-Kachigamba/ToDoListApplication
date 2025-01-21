5eaa8da7d0c4de2b8086018ac3f30c2f375716b6
Harry Kachigamba <harrykachigamba22@outlook.com>
2025-01-21 16:53:25 +0200

    added a viewmodel

--- app/src/main/java/com/example/todolistapplication/operations/ToDoViewModel.kt
+++ app/src/main/java/com/example/todolistapplication/operations/ToDoViewModel.kt
@@ -1,4 +1,39 @@
 package com.example.todolistapplication.operations
 
-class ToDoViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
+import com.example.todolistapplication.data.ToDoDao
+import com.example.todolistapplication.data.ToDoDatabase
+import com.example.todolistapplication.data.ToDoItem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val toDoDao: ToDoDao
+
+    init {
+        val database = Room.databaseBuilder(
+            application,
+            ToDoDatabase::class.java,
+            "todo_database"
+        ).build()
+        toDoDao = database.toDoDao()
+    }
+
+    val toDos: Flow<List<ToDoItem>> = toDoDao.getAllToDos()
+
+    fun addOrUpdateToDo(toDoItem: ToDoItem) {
+        viewModelScope.launch {
+            toDoDao.insertToDo(toDoItem)
+        }
+    }
+
+    fun deleteToDo(toDoItem: ToDoItem) {
+        viewModelScope.launch {
+            toDoDao.deleteToDo(toDoItem)
+        }
+    }
 }
