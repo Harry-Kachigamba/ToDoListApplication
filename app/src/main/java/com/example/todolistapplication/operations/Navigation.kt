82faccf33c5ba64438900778be7afb9faf39de26
Harry Kachigamba <harrykachigamba22@outlook.com>
2025-01-21 16:51:09 +0200

    added navigation setting

--- app/src/main/java/com/example/todolistapplication/operations/Navigation.kt
+++ app/src/main/java/com/example/todolistapplication/operations/Navigation.kt
@@ -1,2 +1,34 @@
 package com.example.todolistapplication.operations
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.todolistapplication.data.ToDoItem
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController, startDestination = "list") {
+        composable("list") {
+            ToDoListScreen(
+                onNavigateToAddEdit = { toDo ->
+                    navController.navigate("addEdit?todoId=${toDo?.id}")
+                }
+            )
+        }
+        composable("addEdit") {
+            AddEditToDoScreen(
+                toDoItem = null,
+                onSave = { toDoItem ->
+                    saveToDoItem(toDoItem)
+                    navController.popBackStack()
+                },
+                onCancel = { navController.popBackStack()}
+            )
+        }
+    }
+}
+
+fun saveToDoItem(toDoItem: ToDoItem) {
+    println("Saving ToDoItem: ${toDoItem.title}, ${toDoItem.description}")
+}
