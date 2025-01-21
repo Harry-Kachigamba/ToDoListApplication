1a5a744a94b8a69d53ddde81bf73c9a5e080e7d1
Harry Kachigamba <harrykachigamba22@outlook.com>
2025-01-21 16:54:05 +0200

    added an edit todo task user interface

--- app/src/main/java/com/example/todolistapplication/operations/AddEditToDoScreen.kt
+++ app/src/main/java/com/example/todolistapplication/operations/AddEditToDoScreen.kt
@@ -1,2 +1,86 @@
 package com.example.todolistapplication.operations
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.todolistapplication.data.ToDoItem
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddEditToDoScreen(
+    toDoItem: ToDoItem?,
+    onSave : (ToDoItem) -> Unit,
+    onCancel: () -> Unit
+) {
+        var title by remember { mutableStateOf(toDoItem?.title ?: "") }
+        val description by remember { mutableStateOf(toDoItem?.description ?: "") }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text("Add/Edit ToDos")
+                    }
+                )
+            },
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                TextField(
+                    value = title,
+                    onValueChange = { title = it },
+                    label = { Text("Title") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                TextField(
+                    value = description,
+                    onValueChange = { title = it },
+                    label = { Text("Description") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(
+                    onClick = {
+                        if (title.isNotEmpty() && description.isNotEmpty()) {
+                            onSave(ToDoItem(title = title, description = description))
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Save")
+                }
+                OutlinedButton(
+                    onClick = onCancel,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Cancel")
+                }
+            }
+        }
+}
